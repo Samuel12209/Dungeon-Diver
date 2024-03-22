55eef6d660a3c971cc2659f6a49041208c4ba481
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-21 22:01:40 -0400

    fixed dungeon placement

--- Dungeon Diver/DungeonDiver.py
+++ Dungeon Diver/DungeonDiver.py
@@ -1,5 +1,6 @@
 import pygame
-import time
+import sys
+
 pygame.init()
 
 PlayerY = 50
@@ -7,46 +8,40 @@ PlayerX = 50
 
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("DungeonDiver")
+
+# Load images
 icon = pygame.image.load('Dungeon Diver\Dungeon Diver.png')
 icon = pygame.transform.scale(icon, (32, 32))
-pygame.display.set_caption("DungeonDiver")
 pygame.display.set_icon(icon)
-dungeonload = pygame.image.load('Dungeon Diver\Dungeon.jpg')
-dungeon = pygame.Surface((600, 600))
+
+dungeon = pygame.image.load('Dungeon Diver\Dungeon.jpg')
 Character1 = pygame.image.load('Dungeon Diver\Character.png')
-Player = pygame.transform.scale(Character1,(96,96))
-
-
+Player = pygame.transform.scale(Character1, (96, 96))
 
 running = True
 while running:
-    
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-  
+
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a] and PlayerX > -15:
         PlayerX -= 0.1
 
-    if keys[pygame.K_d] and PlayerX < 460 :
+    if keys[pygame.K_d] and PlayerX < 460:
         PlayerX += 0.1
 
     if keys[pygame.K_s] and PlayerY < 380:
         PlayerY += 0.1
-    
-    if keys[pygame.K_w] and PlayerY > -10 :
+
+    if keys[pygame.K_w] and PlayerY > -10:
         PlayerY -= 0.1
 
-
-    screen.blit(dungeon, (0,0))
-    screen.blit(Player, (PlayerX,PlayerY))
-  
-
-    pygame.display.update()
-
+    screen.blit(dungeon, (-10, -60))
+    screen.blit(Player, (PlayerX, PlayerY))
+    pygame.display.flip()
 
 pygame.quit()
-quit()
+sys.exit()
