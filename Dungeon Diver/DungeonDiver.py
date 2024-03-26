f7beee4a467fcc822248444aa0b8c1db7ba9903d
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-25 21:25:47 -0400

    left and right movement

--- Dungeon Diver/DungeonDiver.py
+++ Dungeon Diver/DungeonDiver.py
@@ -1,47 +1,66 @@
 import pygame
-import sys
+import time
 
 pygame.init()
 
 PlayerY = 50
 PlayerX = 50
+PlayerWidth = 96
+PlayerHeight = 96
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 620, 500
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("DungeonDiver")
 
-# Load images
 icon = pygame.image.load('Dungeon Diver\Dungeon Diver.png')
 icon = pygame.transform.scale(icon, (32, 32))
+
+pygame.display.set_caption("DungeonDiver")
 pygame.display.set_icon(icon)
 
-dungeon = pygame.image.load('Dungeon Diver\Dungeon.jpg')
+dungeonload = pygame.image.load('Dungeon Diver\Dungeon.jpg').convert()
+dungeonload = pygame.transform.scale(dungeonload, (650, 600))
+
 Character1 = pygame.image.load('Dungeon Diver\Character.png')
 Player = pygame.transform.scale(Character1, (96, 96))
+
+
+obstacle2 = pygame.Rect(0, 250, 190, 20)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    time.sleep(0.001)
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_a] and PlayerX > -15:
-        PlayerX -= 0.1
-
-    if keys[pygame.K_d] and PlayerX < 460:
-        PlayerX += 0.1
-
+    dx, dy = 0, 0
+    if keys[pygame.K_a] and PlayerX > 0:
+        Character1 = pygame.image.load('Dungeon Diver\Characterleft.png')
+        dx -= 2
+    if keys[pygame.K_d] and PlayerX < 500:
+        Character1 = pygame.image.load('Dungeon Diver\Character.png')
+        dx += 2
     if keys[pygame.K_s] and PlayerY < 380:
-        PlayerY += 0.1
+        dy += 0.5
+    if keys[pygame.K_w] and PlayerY > 25:
+        dy -= 0.5
+    
+    if keys[pygame.K_]
 
-    if keys[pygame.K_w] and PlayerY > -10:
-        PlayerY -= 0.1
+    new_player_rect = pygame.Rect(PlayerX + dx, PlayerY + dy, PlayerWidth, PlayerHeight)
+    if not new_player_rect.colliderect(obstacle2) and not new_player_rect.colliderect(obstacle2):
 
-    screen.blit(dungeon, (-10, -60))
+        PlayerX += dx
+        PlayerY += dy
+    
+    Player = pygame.transform.scale(Character1, (96, 96))
+    screen.blit(dungeonload, (-30, -50))
     screen.blit(Player, (PlayerX, PlayerY))
-    pygame.display.flip()
+
+    pygame.display.update()
 
 pygame.quit()
-sys.exit()
+quit()
