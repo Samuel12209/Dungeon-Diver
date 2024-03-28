b097f139e4445120cbb46ef3c426abb14b6fe376
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-27 21:35:13 -0400

    Temp(Knight not working)

--- Dungeon Diver/DungeonDiver.py
+++ Dungeon Diver/DungeonDiver.py
@@ -7,7 +7,6 @@ PlayerY = 50
 PlayerX = 50
 PlayerWidth = 96
 PlayerHeight = 96
-
 WIDTH, HEIGHT = 620, 500
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -24,8 +23,8 @@ dungeonload = pygame.transform.scale(dungeonload, (650, 550))
 Character1 = pygame.image.load('Dungeon Diver\\assets\\Character.png')
 Player = pygame.transform.scale(Character1, (96, 96))
 
-knight = pygame.image.load('Dungeon Diver\\assets\\Knight.png')
-pygame.transform.scale(knight,(64,64))
+knightload = pygame.image.load('Dungeon Diver\\assets\\Knight.png')
+knight = pygame.transform.scale(knightload,(64,64))
 
 obstacle2 = pygame.Rect(0, 250, 190, 20)
 knight1 = pygame.Rect(267, 308, 315, 362)
@@ -38,6 +37,7 @@ slash_frames2 = [
     for i in range(3)
 ]
 health = 100
+knight1x, knight1y = 281, 320
 
 def slash():
   if facing == "Right":
@@ -57,34 +57,37 @@ def slash():
       pygame.display.update()
 fps = pygame.time.Clock()
 
+charleft = pygame.image.load('Dungeon Diver\\assets\\Characterleft.png')
+charright = pygame.image.load('Dungeon Diver\\assets\\Character.png')
+
 running = True
 while running:
   fps.tick(20)
   for event in pygame.event.get():
     if event.type == pygame.MOUSEBUTTONDOWN:
-      slash()
-      print("slash")
-      x, y = pygame.mouse.get_pos()
-      print(x, y)
+        if event.button == 1:
+            slash()
+            print("slash")
+            x, y = pygame.mouse.get_pos()
+            print(x, y)
     if event.type == pygame.QUIT:
       running = False
-  time.sleep(0.001)
   facing = "middle"
   keys = pygame.key.get_pressed()
 
   dx, dy = 0, 0
   if keys[pygame.K_a] and PlayerX > 0:
-    Character1 = pygame.image.load('Dungeon Diver\\assets\\Characterleft.png')
+    Character1 = charleft
     facing = "Left"
-    dx -= 4
+    dx -= 10
   if keys[pygame.K_d] and PlayerX < 500:
-    Character1 = pygame.image.load('Dungeon Diver\\assets\\Character.png')
+    Character1 = charright
     facing = "Right"
-    dx += 4
+    dx += 10
   if keys[pygame.K_s] and PlayerY < 380:
-    dy += 2
+    dy += 5
   if keys[pygame.K_w] and PlayerY > 25:
-    dy -= 2
+    dy -= 5
 
   new_player_rect = pygame.Rect(PlayerX + dx, PlayerY + dy, PlayerWidth,
                                 PlayerHeight)
@@ -93,27 +96,26 @@ while running:
     PlayerX += dx
     PlayerY += dy
 
-    
   if knight1.colliderect(new_player_rect):
     health -= 5
     print("Hit! -5 hp")
     print(str(health) + "Left!")
     if health <= 0:
       print("You died!")
-  if health <= 0: 
+  if health <= 0:
     deathscreen = pygame.image.load('Dungeon Diver\\assets\\You died!.png')
-    pygame.transform.scale(deathscreen,(WIDTH, HEIGHT))
-    screen.blit(deathscreen, (0, 0))
+    pygame.transform.scale(deathscreen,(800, 800))
+    screen.blit(deathscreen, (-30, -50))
     pygame.display.update()
-    time.sleep(10)
+    time.sleep(3)
     running = False
-
 
   Player = pygame.transform.smoothscale(Character1, (96, 96))
   screen.blit(dungeonload, (-30, -50))
-  screen.blit(knight, (knight1x, knight1y))
+  screen.blit(knightload, (knight1x,knight1y))
   screen.blit(Player, (PlayerX, PlayerY))
   pygame.display.update()
 
+print("Knight Position:", knight1x, knight1y)
 pygame.quit()
 quit()
